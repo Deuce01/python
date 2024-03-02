ce20388c2a38d444ac19de349756c8e2ef65fe8a
VICTOR CHEGE <98538799+Deuce01@users.noreply.github.com>
2024-03-02 18:25:27 +0300

    Create UTC.py

---
+++ UTC.py
@@ -0,0 +1,13 @@
+# Import datetime module using the import keyword.
+import datetime
+
+# Get the current date by calling the today() function of the datetime module
+# Store it in a variable
+current_date = datetime.date.today()
+# Get the current date and time by calling the now() function of the datetime module
+# Store it in another variable
+current_time = datetime.datetime.now()
+# Print the current date
+print("The current date:", current_date)
+# Print the current date and time
+print("The current date and time:", current_time)
